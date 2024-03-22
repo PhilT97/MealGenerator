adf8b0c8d2343c753377eee459ce1b01649cbe21
Philipp Tschan <jptschan@gmx.de>
2024-03-22 17:46:27 +0100

    Initial Commit

---
+++ MealGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealGenerator
+//
+//  Created by Philipp Tschan on 22.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
