37c1a5914cce2f2191f1cdd8ee2882e129128bb4
Philipp Tschan <jptschan@gmx.de>
2024-03-22 19:25:37 +0100

    Implemented GPT functionality

--- MealGenerator/ViewController.swift
+++ MealGenerator/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var gptInput:String!
+    
+    @IBOutlet var testButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        gptInput = testButton.currentTitle
+        GPTManager.shared.fetchGPTResponse(prompt: gptInput) { response in
+                    DispatchQueue.main.async {
+                        // Stelle sicher, dass du UI-Updates auf dem Hauptthread durchführst
+                        if let response = response {
+                            print("GPT-Antwort: \(response)")
+                            // Verarbeite die Antwort oder aktualisiere deine UI entsprechend
+                        } else {
+                            print("Es gab ein Problem bei der Anfrage.")
+                        }
+                    }
+                }
+    }
+    
+    
+    
 
 }
 
