9e77a19265cfa535649cbeb9e878121e92b3acbd
Philipp Tschan <jptschan@gmx.de>
2024-03-25 21:47:06 +0100

    Implemented additional ingredient entries in Table View

--- MealGenerator/MealTypeVC.swift
+++ MealGenerator/MealTypeVC.swift
@@ -9,24 +9,47 @@ import Foundation
 import OpenAIKit
 import UIKit
 
-class MealTypeVC: UIViewController {
+class MealTypeVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+    
+    
     
     private let lightBlue = UIColor(red: 213.0/255.0, green: 230.0/255.0, blue: 253/255.0, alpha: 1)
+    
+    
+    // Custom Preset View
+    var dataEntries: [dataEntry] = []
+    @IBOutlet var plusButton: UIButton!
+    @IBOutlet var minusButton: UIButton!
+    @IBOutlet var customPresetTextField: UITextField!
+    
+    @IBOutlet var initialTableCell: UITableViewCell!
+    
+    
+    
+    @IBOutlet var customPresetInputView: UIView!
+    private var customPresetInputViewY: CGFloat!
+    
     
     
     // View-Outlets in Current View Controller
     
     @IBOutlet var mealTypeView: UIView!
     @IBOutlet var mealTimeView: UIView!
+    @IBOutlet var customPresetView: UIView!
     
     
     @IBOutlet var transparancyView: UIView!
     @IBOutlet var generateRecipeView: UIView!
     
+    @IBOutlet var categoryViews: [UIView]!
+    
     
     // Button Bar Button Outlets
     @IBOutlet var mealTypeButton: UIButton!
     @IBOutlet var mealTimeButton: UIButton!
+    @IBOutlet var customPresetButton: UIButton!
+    
+    
     
     @IBOutlet var generateRecipeButton: UIButton!
     
@@ -54,6 +77,7 @@ class MealTypeVC: UIViewController {
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
     
+    @IBOutlet var customPresetTableView: UITableView!
     
     
     
@@ -65,8 +89,10 @@ class MealTypeVC: UIViewController {
         super.viewWillAppear(animated)
         
         generateRecipeView.layer.cornerRadius = 20
-        
-        buttonBarButtons = [mealTypeButton, mealTimeButton]
+        // Initiate View Array
+        categoryViews = [mealTypeView, mealTimeView, customPresetView]
+        // initiate Button Arrays
+        buttonBarButtons = [mealTypeButton, mealTimeButton, customPresetButton]
         mealTypeButtons = [meatButton, vegetarianButton, veganButton, randomType]
         for button in mealTypeButtons {
             button.backgroundColor = UIColor.clear
@@ -83,22 +109,117 @@ class MealTypeVC: UIViewController {
         
         
         
+        
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        
     }
     
     override func viewDidLoad() {
         // initialize buttons in array
+        self.customPresetInputViewY = customPresetInputView.frame.origin.y
         
         
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // keyboard functionality
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        
+        customPresetTextField.delegate = self
+        
+        // Table functionality
+        customPresetTableView.dataSource = self
+        customPresetTableView.delegate = self
+        
         
     }
     
+    // Keyboard Methods
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {return}
+        let newViewPosition = keyboardSize.height
+        
+        UIView.animate(withDuration: 0.3){
+            self.customPresetInputView.frame.origin.y = newViewPosition - self.customPresetInputView.frame.height
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.customPresetInputView.frame.origin.y = self.customPresetInputViewY
+            self.view.layoutIfNeeded()
+            
+        }
+    }
 
     
     
+}
+
+
+// Table Methods
+extension MealTypeVC {
+    
+    @IBAction func cellPlusButtonTapped(){
+        guard let text = customPresetTextField.text, !text.isEmpty else {return}
+        dataEntries.append(dataEntry(text: text, sign: "+"))
+        customPresetTableView.reloadData()
+        customPresetTextField.text = ""
+       
+    }
+    
+    @IBAction func cellMinusButtonTapped(){
+        guard let text = customPresetTextField.text, !text.isEmpty else {return}
+        dataEntries.append(dataEntry(text: text, sign: "-"))
+        customPresetTableView.reloadData()
+        customPresetTextField.text = ""
+    }
+    
+    
+    // Data source Methods
+    // Row Count in each section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataEntries.count
+    }
+    
+    // Cell configuration
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = customPresetTableView.dequeueReusableCell(withIdentifier: "customIngredientCellID", for: indexPath)
+        let entry = dataEntries[indexPath.row]
+        cell.textLabel?.text = "\(entry.sign) \(entry.text)"
+        return cell
+
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            dataEntries.remove(at: indexPath.row)
+            customPresetTableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
 
 // BUTTON BAR METHODS
@@ -108,8 +229,9 @@ extension MealTypeVC {
         if self.mealTimeView.alpha != 1 {
             UIView.animate(withDuration: 0.5) {
                 self.unselect_buttonBarButtons()
-                self.mealTypeView.alpha = 0
+                self.hide_views()
                 self.mealTimeView.alpha = 1
+                self.mealTimeView.isHidden = false
                 self.mealTimeButton.isSelected = true
             }
         }
@@ -120,9 +242,22 @@ extension MealTypeVC {
         if self.mealTypeView.alpha != 1 {
             UIView.animate(withDuration: 0.5) {
                 self.unselect_buttonBarButtons()
-                self.mealTimeView.alpha = 0
+                self.hide_views()
                 self.mealTypeView.alpha = 1
+                self.mealTypeView.isHidden = false
                 self.mealTypeButton.isSelected = true
+            }
+        }
+    }
+    
+    @IBAction func customPresetButtonTapped(_ sender: UIButton){
+        if self.customPresetView.alpha != 1 {
+            UIView.animate(withDuration: 0.5) {
+                self.unselect_buttonBarButtons()
+                self.hide_views()
+                self.customPresetView.alpha = 1
+                self.customPresetView.isHidden = false
+                self.customPresetButton.isSelected = true
             }
         }
     }
@@ -138,7 +273,7 @@ extension MealTypeVC {
     }
     
     
-    
+    // Button helpers
     func unselect_buttonBarButtons(){
         for button in self.buttonBarButtons {
             button.isSelected = false
@@ -152,6 +287,16 @@ extension MealTypeVC {
         }
         return is_active
     }
+    
+    // View Helper
+    func hide_views() {
+        for v in self.categoryViews {
+            v.alpha = 0
+            v.isHidden = true
+        }
+    }
+    
+    
     
 }
 
@@ -349,6 +494,13 @@ extension MealTypeVC {
     
     
     
+}
+
+// TABLE DATA MODEL
+
+struct dataEntry {
+    var text: String
+    var sign: String
 }
 
 //
